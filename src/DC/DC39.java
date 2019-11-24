7f30b5bef73d9ade83125624275314b5d540c52b
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-24 23:06:14 +0530

    Default attribute added

---
+++ src/DC/DC39.java
@@ -0,0 +1,11 @@
+package DC;
+
+class DC39 {
+    String name = "Deepak";
+    int age = 21;
+
+    public static void main(String[] args) {
+        DC39 dc39 = new DC39();
+        System.out.println("Name - " + dc39.name + "\nAge - " + dc39.age);
+    }
+}
