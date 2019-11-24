4d38993a35300ceaeff100caee146b8827c45f0c
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-24 22:50:01 +0530

    Finally

---
+++ src/DC/DC38.java
@@ -0,0 +1,12 @@
+package DC;
+
+public class DC38 {
+    private String name = "Deepak";
+    private int age = 21;
+    private String mailId = "choudharydeepak990@gmail.com";
+
+    public static void main(String[] args) {
+        DC38 myObj = new DC38();
+        System.out.println("Name = " + myObj.name + "\nAge = " + myObj.name + "\nMail ID = " + myObj.mailId);
+    }
+}
---
+++ src/DC/Main.java
@@ -0,0 +1,10 @@
+package DC;
+
+public class Main {
+    String name = "Deepak";
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        myObj.name = "Hello";
+        System.out.println("Deepak" + myObj.name);
+    }
+}
