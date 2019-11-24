d9720b8a35d9d84352685255d699cd23376eebb7
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-24 23:19:21 +0530

    Final class added which revoke inheritance

---
+++ src/DC/DC40.java
@@ -0,0 +1,11 @@
+package DC;
+
+class DC40 {
+    protected String name = "Deepak";
+    protected int age = 21;
+
+    public static void main(String[] args) {
+        DC40 dc40 = new DC40();
+        System.out.println("Name - " + dc40.name + "\nAge - " + dc40.age);
+    }
+}
---
+++ src/DC/DC41.java
@@ -0,0 +1,10 @@
+package DC;
+
+final class DC41 {
+    String name = "Deepak chodhdary";
+
+    public static void main(String[] args) {
+        DC41 dc41 = new DC41();
+        System.out.println("Name :- " + dc41.name);
+    }
+}
