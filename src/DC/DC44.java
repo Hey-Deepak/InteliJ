1f1f11d2fbf11bff53c5b9d485b3fd5e6a291c48
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-26 03:15:52 +0530

    java API imported

---
+++ src/DC/DC44.java
@@ -0,0 +1,16 @@
+package DC;
+
+import java.util.Scanner;
+
+public class DC44 {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Scanner myobj = new Scanner(System.in);
+        String username;
+        System.out.println("Enter Username:-");
+        username = myobj.nextLine();
+        System.out.println("Name is " + username);
+    }
+}
