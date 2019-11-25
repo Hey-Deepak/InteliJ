8b80955ed252ea1ace1ea68f20260c17c9e981b1
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-26 03:55:54 +0530

    java API imported for class

--- src/DC/DC44.java
+++ src/DC/DC44.java
@@ -3,8 +3,7 @@ package DC;
 import java.util.Scanner;
 
 public class DC44 {
-    String name;
-    int age;
+
 
     public static void main(String[] args) {
         Scanner myobj = new Scanner(System.in);
