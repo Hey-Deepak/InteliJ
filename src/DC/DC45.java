9b3665959492534e7e04c2179d376e1fc80fd922
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-26 03:56:38 +0530

    package imported

---
+++ src/DC/DC45.java
@@ -0,0 +1,15 @@
+package DC;
+
+import java.util.*;
+
+public class DC45 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String usename;
+        System.out.println("Enter Username:-");
+        usename = scanner.nextLine();
+        System.out.println("Username :- " + usename);
+    }
+
+}
