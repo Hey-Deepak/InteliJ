40463ddeaaa065cdaf3babb1efc0568690ca9145
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:48:58 +0530

    for each Loop through ArrayList.

---
+++ src/DC/DC59.java
@@ -0,0 +1,15 @@
+package DC;
+
+import java.util.ArrayList;
+
+public class DC59 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<String>();
+        myList.add("Deepak");
+        myList.add("Choudhary");
+        for (String i : myList) {
+            System.out.println(i);
+        }
+    }
+}
+
