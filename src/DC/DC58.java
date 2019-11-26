e1946e795c1d7e6d2cf03075fb30170c5b6b108a
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:53:51 +0530

    Integer ArrayList Created .

--- src/DC/DC58.java
+++ src/DC/DC58.java
@@ -1,4 +1,12 @@
 package DC;
 
+import java.util.ArrayList;
 public class DC58 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<Integer>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        System.out.println(myList);
+    }
 }
