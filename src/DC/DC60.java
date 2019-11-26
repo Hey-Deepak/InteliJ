1a0652ef0a0d44bf545f4f09ecdd26a4da1cf5a3
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:59:54 +0530

    try catch Statement.

---
+++ src/DC/DC60.java
@@ -0,0 +1,13 @@
+package DC;
+
+public class DC60 {
+    public static void main(String[] args) {
+        try {
+            int[] myInt = {1, 2, 3};
+            System.out.println(myInt[1]);
+        } catch (Exception e) {
+            System.out.println("Something went Wrong");
+        }
+        ;
+    }
+}
