06ba63541d989566c77f3c7bd6b177c1df7aa66a
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 01:52:14 +0530

    adding to object to each other

--- src/DC/DC47.java
+++ src/DC/DC47.java
@@ -1,4 +1,46 @@
 package DC;
 
+import java.util.List;
+
 public class DC47 {
+
+    public static void add(int a, int b) {
+        System.out.println("called add(int a, int b) : " + a + b);
+    }
+
+    public static void add(int a) {
+        System.out.println("called add(int a) : " + a + 5);
+    }
+
+    public static void add(float a) {
+        System.out.println("called add(float a) : " + a + 5);
+    }
+
+    public static void add(float a, float b) {
+        System.out.println("called add(float a, float b) : " + a + b);
+    }
+
+    public static class A {
+        int a;
+
+        public A(int a) {
+            this.a = a;
+        }
+
+        @Override
+        public String toString() {
+            return "myint:" + a;
+        }
+    }
+
+    public static void main(String args[]) {
+        A a = new A(5), b = new A(6);
+        System.out.println(a + " " + b);
+
+        /*add(5);
+        add(4,6);
+        add(4.5f,6.6f);
+        add(4.5f);*/
+    }
+
 }
