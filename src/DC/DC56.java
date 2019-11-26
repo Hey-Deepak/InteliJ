91b9f595b725256ca935afc24fa6f39344c2a629
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:39:12 +0530

    ArrayList accessed by get() function.

---
+++ src/DC/DC56.java
@@ -0,0 +1,12 @@
+package DC;
+
+import java.util.ArrayList;
+
+public class DC56 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<String>();
+        myList.add("Deepak");
+        myList.add("Choudhary");
+        System.out.println(myList.get(1));
+    }
+}
