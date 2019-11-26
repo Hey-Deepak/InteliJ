d783100ba6a31d71b843baf5c9fec85ec037fac0
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:34:40 +0530

    ArrayList created

---
+++ src/DC/DC55.java
@@ -0,0 +1,12 @@
+package DC;
+
+import java.util.ArrayList;
+
+public class DC55 {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<String>();
+        myList.add("Deepak");
+        myList.add("Choudhary");
+        System.out.println(myList);
+    }
+}
