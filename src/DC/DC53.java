a93eb1bca3a621e49bf030dd0d104dbe4899592a
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 03:06:59 +0530

    Date and Time added for Now().

---
+++ src/DC/DC53.java
@@ -0,0 +1,10 @@
+package DC;
+
+import java.time.LocalDateTime;
+
+public class DC53 {
+    public static void main(String[] args) {
+        LocalDateTime myTimeDate = LocalDateTime.now();
+        System.out.println("Time and Date :- " + myTimeDate);
+    }
+}
