3658aa8b922e0e832777cfd6ece18b71969c701e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 04:51:07 +0530

    converted wrapper object to String

---
+++ src/DC/DC64.java
@@ -0,0 +1,9 @@
+package DC;
+
+public class DC64 {
+    public static void main(String[] args) {
+        Integer myInt = 10000;
+        String myString = myInt.toString();
+        System.out.println(myString.length());
+    }
+}
