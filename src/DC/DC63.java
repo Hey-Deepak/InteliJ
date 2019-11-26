931e54b8ec4bc26d50455683c20213f400fc809e
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-11-27 04:47:34 +0530

    wrapper method added

---
+++ src/DC/DC63.java
@@ -0,0 +1,11 @@
+package DC;
+
+public class DC63 {
+    Integer myInt = 10;
+
+    public static void main(String[] args) {
+        DC63 myObj = new DC63();
+        System.out.println(myObj.myInt.intValue());
+
+    }
+}
